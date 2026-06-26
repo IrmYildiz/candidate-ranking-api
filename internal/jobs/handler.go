df7ce2eab1e200e88aada4497ecaa05bd45ba154
Irem Yildiz <iremyildiz@hotmail.co.uk>
2026-06-26 21:28:56 +0100

    Creating files for logic and http layer for when jobs are created

---
+++ internal/jobs/handler.go
@@ -0,0 +1,8 @@
+package jobs
+
+import "net/http"
+
+func jobHandler(w http.ResponseWriter, r *http.Request) {
+	// Function to handle:
+	// POST, GET all jobs, GET job by ID
+}
---
+++ internal/jobs/service.go
@@ -0,0 +1,13 @@
+package jobs
+
+func createJob() {
+	// Function for admin to create a new job
+}
+
+func getJobs() {
+	// Function to get a list of all jobs an admin has created
+}
+
+func getJobByID() {
+	// Function to get a job by its unique ID
+}
